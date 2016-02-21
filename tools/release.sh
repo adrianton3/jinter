eeb8519d82d7ca924075c5d7f2aa9da783473e3d
Adrian Toncean <adrianton3@gmail.com>
2016-02-21 11:34:16 +0100

    Update the release script

--- tools/release.sh
+++ tools/release.sh
@@ -6,11 +6,13 @@ cp -r src tmp
 cp -r demo tmp
 mkdir tmp/snippets
 cp test/unit/spec/snippets/*.js tmp/snippets
+cp test/unit/meta.js tmp
 
 git checkout gh-pages
 
 cp -r tmp/src ./
 cp -r tmp/demo ./
-cp tmp/snippets test/unit/spec
+cp -r tmp/snippets test/unit/spec
+cp tmp/meta.js test/unit
 
 rm -rf tmp
